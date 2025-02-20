6587e2afa2d5343fb185d74e4b976c596733a85f
Apurav <apurav0711@gmail.com>
2025-02-20 16:51:03 +0530

    modified:   app.py 	new file:   models/all-MiniLM-L6-v2/1_Pooling/config.json 	new file:   models/all-MiniLM-L6-v2/README.md 	new file:   models/all-MiniLM-L6-v2/config.json 	new file:   models/all-MiniLM-L6-v2/config_sentence_transformers.json 	new file:   models/all-MiniLM-L6-v2/model.safetensors 	new file:   models/all-MiniLM-L6-v2/modules.json 	new file:   models/all-MiniLM-L6-v2/sentence_bert_config.json 	new file:   models/all-MiniLM-L6-v2/special_tokens_map.json 	new file:   models/all-MiniLM-L6-v2/tokenizer.json 	new file:   models/all-MiniLM-L6-v2/tokenizer_config.json 	new file:   models/all-MiniLM-L6-v2/vocab.txt 	modified:   notebook.ipynb

--- app.py
+++ app.py
@@ -5,16 +5,22 @@ import langchain
 from langchain_community.document_loaders import UnstructuredURLLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.chains import RetrievalQAWithSourcesChain
 
 st.title("Question Answering Bot")
 
-llm = ChatGroq(
-    model_name = "llama-3.3-70b-versatile",
-    temperature = 0.9,
-    groq_api_key = "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"
-)
+def get_llm():
+    llm = ChatGroq(
+        model_name = "llama-3.3-70b-versatile",
+        temperature = 0.9,
+        groq_api_key = "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"
+    )
 
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return llm
+
+def load_embedder():
+    model = HuggingFaceEmbeddings(model_name="./models/all-MiniLM-L6-v2");
+    return model
 
 def output (urls, question):
     try:
@@ -28,13 +34,17 @@ def output (urls, question):
 
         text = text_splitter.split_documents(doc)
 
+        vectorIndex = FAISS.from_documents(text, embeddings)
+
+        chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorIndex.as_retriever())
+
+        output = chain({"question": question}, return_only_outputs=True)
+
+        return output['answer']
+
 
     except Exception as e:
         return str(e)
-
-
-
-
 
 question = st.text_input("Enter Your Question")
 # st.sidebar.title("Enter Your Urls : ")
@@ -43,3 +53,28 @@ url1 = st.sidebar.text_input("Enter Your Url1 ")
 
 url2 = st.sidebar.text_input("Enter Your Url2 ")
 url3 = st.sidebar.text_input("Enter Your Url3 ")
+
+button = st.button('Give Answer')
+
+if button:
+    # result = output([url1, url2, url3], question)
+    if(not question):
+        st.write("Please enter a question.")
+    else: 
+        llm = get_llm()
+        embeddings = load_embedder()
+
+        urls = []
+        if(url1):
+            urls.append(url1)
+        if(url2):
+            urls.append(url2)
+        if(url3):
+            urls.append(url3)
+        
+        if(not urls):
+            st.write("Please enter at least one URL.")
+        else:
+            result = output(urls, question)
+            st.write(result)
+
